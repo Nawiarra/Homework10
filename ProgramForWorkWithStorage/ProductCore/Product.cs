9530143f1311b064abd5c38846a84cb135e2bbee
Nawiarra <nika.berezovskaya@ukr.net>
2020-08-29 17:56:04 +0300

    Add storage core and change  baseProduct security clearance

--- ProgramForWorkWithStorage/ProductCore/Product.cs
+++ ProgramForWorkWithStorage/ProductCore/Product.cs
@@ -11,13 +11,18 @@ namespace ProductCore
         public string name { get; }
         public decimal price { get; set; }
 
-        BaseProduct baseProduct;
+        public BaseProduct baseProduct;
 
         Product(string name, decimal price, DateTime dateOfDelivery, int countDayToExpirationDate)
         {
             this.name = name;
             this.price = price;
             this.baseProduct = new BaseProduct(dateOfDelivery, countDayToExpirationDate);
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.name}.\nPrice: {this.price}.\n Expiration Date: {this.baseProduct.ShowExpirationDate()} ";
         }
 
     }
---
+++ ProgramForWorkWithStorage/StorageCore/Storage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProductCore;
+
+namespace StorageCore
+{
+    public class Storage
+    {
+        private int volume;
+
+        private List<Product> ProductStorage = new List<Product>();
+
+        public string CheckProductExpiryDate(Product checkingProduct)
+        {
+            DateTime checkingDate = checkingProduct.baseProduct.ShowExpirationDate();
+
+            if(checkingDate <= DateTime.Now)
+            {
+                ProductStorage.Remove(checkingProduct);
+
+                return "This thing expired and will decommission";
+            }
+
+            return "This thing not expired";
+        }
+
+        public List<string> ShowListOfProduct()
+        {
+            List<string> productInfo = new List<string>();
+
+            foreach (Product product in ProductStorage)
+            {
+                productInfo.Add(product.ToString());
+            }
+
+            return productInfo;
+        }
+
+        public void AddProductToStorage(Product productForAdd)
+        {
+            ProductStorage.Add(productForAdd);
+        }
+
+        public void DeleteProductToStorage(Product productForDelete)
+        {
+            ProductStorage.Remove(productForDelete);
+        }
+
+    }
+}
